0a205643daa907378110bd6c46323108ffd1d9c1
ryansmith <ryansmith@ryans-air.lan1>
2022-08-15 18:28:07 -0700

    Quiz Game and Awesome or Not

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -10,7 +10,7 @@ public class QuizGame {
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-		String response1 = JOptionPane.ShowInputDialog(null, "What planet is closest to the sun?");
+		String response1 = JOptionPane.showInputDialog(null, "What planet is closest to the sun?");
 				// 3.  Use an if statement to check if their answer is correct
 				if(response1.equalsIgnoreCase("Mercury")) {
 				score += 1;
@@ -22,16 +22,26 @@ public class QuizGame {
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
 		
-				String response2 = JOptionPane.ShowInputDialog(null, "What was the first Pixar film?");
-			if(response2.equalsIgnoreCase("Toy Story")); {
-			score += 1;}
-			
-			else { score -= 1;}
+		String response2 = JOptionPane.showInputDialog(null, "What was the first Pixar film?");
+				if(response2.equalsIgnoreCase("Toy Story")) {
+					score += 1;
+				}
+				else { score -= 1;
+				}
 		
-		String response3 = JOptionPane.showInputDialog(null, "")
+		String response3 = JOptionPane.showInputDialog(null, "What element has the atomic number one?");
+				if(response3.equalsIgnoreCase("Hydrogen")) {
+					score += 1;
+				}
+				else {score -= 1;
+				}
 			
 		// After all the questions have been asked, tell the user their final score 
-		
-	
+	if(score >= 3) {	
+	JOptionPane.showMessageDialog(null, "Your final score is " + score + ". You answered all of the questions correctly!");
+	}
+	else {
+	JOptionPane.showMessageDialog(null, "Your final score is" + score + ". You did not answer all of the questions correctly.");
+	}
 }
 }
